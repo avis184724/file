c72000e580993d26133a6c14d0e6e7922e8e3f60
SMART24\User <annyunghasam1@gmail.com>
2025-04-14 11:05:59 +0900

    4/14

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -65,6 +65,13 @@ class MyDrawer extends StatelessWidget {
             onTap: () {
               Navigator.pushNamed(context, "/list");
             },
+          ),
+          ListTile(
+            leading: const Icon(Icons.list),
+            title: const Text("To-Do 예제"),
+            onTap: () {
+              Navigator.pushNamed(context, "/to-do");
+            },
           )
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:untitled/list_page.dart';
 import 'package:untitled/popup_page.dart';
 import 'package:untitled/setting_page.dart';
 
+import 'ToDoPage.dart';
 import 'button_page.dart';
 import 'image_page.dart' show ImagePage;
 import 'input_page.dart';
@@ -35,6 +36,7 @@ class MyApp extends StatelessWidget {
         "/button": (context) => const ButtonPage(),
         "/input": (context) => const InputPage(),
         "/list": (context) => const ListPage(),
+        "/to-do": (context) => const ToDoPage(),
       },
     );
   }
